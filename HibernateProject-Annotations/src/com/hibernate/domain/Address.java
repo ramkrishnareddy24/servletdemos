75415b9128eb6baaf523f1bc366e3010396e2f9f
Ramakrishna G. Kannari <rkannari@BLR.ALLEGISINDIA.COM>
2023-03-21 10:25:52 +0530

    PersonAddressHibernate

---
+++ HibernateProject-Annotations/src/com/hibernate/domain/Address.java
@@ -0,0 +1,21 @@
+package com.hibernate.domain;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class Address {
+	private String street;
+	private String city;
+	private String pincode;
+	
+	public Address() {}
+
+	public Address(String street, String city, String pincode) {
+		super();
+		this.street = street;
+		this.city = city;
+		this.pincode = pincode;
+	}
+	
+	
+}
--- PersonAddressHibernateDemo/src/com/hibernate/HibernateDemo.java
+++ PersonAddressHibernateDemo/src/com/hibernate/HibernateDemo.java
@@ -2,8 +2,7 @@ package com.hibernate;
 
 import org.hibernate.Session;
 
-import com.hibernate.domain.Address;
-import com.hibernate.domain.Person;
+import com.hibernate.domain.Message;
 import com.hibernate.utils.HibernateUtil;
 
 public class HibernateDemo {
@@ -12,10 +11,9 @@ public class HibernateDemo {
 		
 		session.beginTransaction();
 		
-		Address address1 = new Address("1124 Home street", "Mumbai", "600123");
-		Address address2 = new Address("2124 Home street", "Chennai", "700123");
-		Person person = new Person("Max", address1,address2);
-		session.save(person);
+		Message message = new Message("HelloWolrd from Hibernate-Annotations");
+		
+		session.save(message);
 		session.getTransaction().commit();
 		session.close();
 	}
